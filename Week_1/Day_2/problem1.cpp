0c77978282001302c01c157ddda33efabb8475c2
DEEPANSHU <deepanshuaggarwal98550@gmail.com>
2026-06-02 11:37:16 +0530

    Update problem1.cpp

--- Week_1/Day_2/problem1.cpp
+++ Week_1/Day_2/problem1.cpp
@@ -1,1 +1,14 @@
-aa
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currsum =0 , maxsum = INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            currsum = currsum + nums[i];
+            maxsum = max(currsum,maxsum);
+            if(currsum<0){
+                currsum =0;
+            }
+        }
+        return maxsum;
+    }
+};
