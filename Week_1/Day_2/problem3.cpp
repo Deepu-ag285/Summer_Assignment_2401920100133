bfb9c513ccd0937378bf287ddbb7bb08699c9912
DEEPANSHU <deepanshuaggarwal98550@gmail.com>
2026-06-02 11:36:44 +0530

    Update problem3.cpp

--- Week_1/Day_2/problem3.cpp
+++ Week_1/Day_2/problem3.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int maxsum = 0;
+        int currsum = 0;
+        for(int i = 0;i<k ; i++){
+            currsum += nums[i];
+        }
+        maxsum = currsum;
+        for(int i =k; i<nums.size();i++){
+            currsum += nums[i]-nums[i-k];
+            if(currsum>maxsum)
+            maxsum = currsum;
+        }
+        return (double)maxsum/k;
+        
+    }
+};
